469ce7a9a88364791d86669549ef7d5843621c16
Sarah Licea <slice@Sarahs-MacBook-Air.local>
2016-10-30 04:02:38 -0700

    Solution to #28 in 0 milliseconds

---
+++ 028.hs
@@ -0,0 +1,15 @@
+-- i = index
+-- a = sum accumulator
+-- x = target for next diagonal
+-- y = target for next sublating diagonal
+-- z = space between targets for current ring
+
+sumSpiralDiag s = go 1 0 1 1 0
+  where go i a x y z
+          | i == (s^2 + 1) = a
+          | i == y = go (succ i) (a + i) (x + (z + 2)) (y + ((z + 2) * 4)) (z + 2)
+          | i == x = go (succ i) (a + i) (x + z) y z
+          | otherwise = go (succ i) a x y z
+
+
+main = print $ sumSpiralDiag 1001
