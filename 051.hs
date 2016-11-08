d0a66b8c67672913498ff4a46c722cf9e8a2526d
Coleman <jackson@gariety.xxx>
2016-11-08 00:13:39 -0800

    OMG it gave the correct answer. I can't believe it. Shooting in the dark!

--- 051.hs
+++ 051.hs
@@ -1,4 +1,6 @@
 import Data.Numbers.Primes
+import Data.List
+import Data.Ord
 
 -- this one is forwards
 fromDigits :: Integral x => [x] -> x
@@ -11,11 +13,15 @@ toDigits x = toDigits (a) ++ [b]
   where (a, b) = quotRem x 10
 
 replaceDigits ds xs = map replace [0..9]
-  where replace n = map (\(i, a) -> if elem i ds then n else a) (zip [0..(length xs)] xs)
+  where replace n = map (\(i, a) -> if elem i ds
+                                    then n
+                                    else a) (zip [1..(length xs)] xs)
 
 length' n = length (show n)
 
-primeDigitReplacements n = take 1 digitalPrimes
-  where digitalPrimes = [toDigits x | x <- [56003]]
+primeDigitReplacements n = last . sortBy (comparing length) . concat $ take 20000 primeFamilies
+  where primeFamilies = [families . toDigits $ x | x <- primes]
+        seqs ls = tail $ subsequences [1..(length ls)]
+        families xs = [filter isPrime . map fromDigits $ replaceDigits ds xs | ds <- (seqs xs)]
 
 main = print . primeDigitReplacements $ 7
