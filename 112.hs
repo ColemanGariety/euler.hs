99ebd54eed27a8c1c53d9a32e740f889ae38484a
Coleman <jackson@gariety.xxx>
2016-11-26 20:32:16 -0800

    Type decs

--- 112.hs
+++ 112.hs
@@ -2,6 +2,7 @@ import Data.List
 import Data.Tuple
 import Debug.Trace
 
+isBouncy :: Integral a => a -> Bool
 isBouncy n = go n False False
   where go n inc dec = let a = div (mod n 100) 10
                            b = mod n 10
@@ -9,6 +10,7 @@ isBouncy n = go n False False
                           then inc && dec
                           else go (div n 10) (a > b || inc) (a < b || dec)
 
+bouncyDensity :: (Ord a, Num a, Integral b, Enum a) => a -> b
 bouncyDensity d = snd (head (snd (break (\(a,b) -> a >= (d * (fromIntegral b))) (zip [1..] (filter isBouncy [1..])))))
 
 main = print (bouncyDensity 0.99)
