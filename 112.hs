7b551f7e9edf76064c5831cc3d6bb9df6fc6e115
Coleman <jackson@gariety.xxx>
2016-11-26 20:31:21 -0800

    Solution to 112

---
+++ 112.hs
@@ -0,0 +1,14 @@
+import Data.List
+import Data.Tuple
+import Debug.Trace
+
+isBouncy n = go n False False
+  where go n inc dec = let a = div (mod n 100) 10
+                           b = mod n 10
+                       in if n < 10
+                          then inc && dec
+                          else go (div n 10) (a > b || inc) (a < b || dec)
+
+bouncyDensity d = snd (head (snd (break (\(a,b) -> a >= (d * (fromIntegral b))) (zip [1..] (filter isBouncy [1..])))))
+
+main = print (bouncyDensity 0.99)
